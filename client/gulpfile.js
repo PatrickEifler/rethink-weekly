412f82c477e045d1daa42a61a6042e6ed5359c06
Vinh <kurei@axcoto.com>
2015-09-16 21:07:53 -0700

    meta

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -10,12 +10,19 @@ var path = {
   JS: ['src/js/*.js', 'src/js/**/*.js'],
   MINIFIED_OUT: 'build.min.js',
   DEST_SRC: 'dist/src',
-  DEST_BUILD: 'dist/build',
+  DEST_BUILD: '../public/dist/build',
   DEST: 'dist'
 }
 
 gulp.task('transform', function(){
   gulp.src(path.JS)
-  .pipe(react())
-  .pipe(gulp.dest(path.DEST_SRC));
+    .pipe(react())
+    .pipe(gulp.dest(path.DEST_SRC))
 })
+
+gulp.task('copy', function(){
+  gulp.src(path.HTML)
+    .pipe(gulp.dest(path.DEST))
+})
+
+
