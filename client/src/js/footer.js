35f064e539adf97c2a6dbb23baf1925501360bf2
Vinh <kurei@axcoto.com>
2015-10-06 12:34:07 -0700

    Write first test ever for component

--- client/src/js/footer.js
+++ client/src/js/footer.js
@@ -4,8 +4,16 @@ export default React.createClass({
   render: function(){
     return (
       <footer>
-        <p>By <a>kureikain</a>.
-        Support me by buying my book <a href="http://leanpub.com/simplyrethinkdb">Simply RethinkDB</a>
+        <p>
+          By <a href="http://noty.im">kureikain</a>.
+          Support me by buying my book <a href="http://leanpub.com/simplyrethinkdb">Simply RethinkDB</a>
+          <nav class="nav">
+            <ul>
+              <li class="menu-item"><a href="#">Home</a></li>
+              <li class="menu-item"><a href="#">Issues</a></li>
+              <li class="menu-item"><a href="#">About</a></li>
+            </ul>
+          </nav>
         </p>
       </footer>
     )
---
+++ client/test/js/footer.js
@@ -0,0 +1,51 @@
+import {
+  React,
+  sinon,
+  assert,
+  expect,
+  TestUtils
+} from '../test_helper';
+
+import Footer from '../../src/js/footer'
+
+describe('Footer component', () => {
+ let data = {
+    form: {
+      formAttrs: {
+        email: {
+          label: "Email Address",
+          type: "email",
+          value: "foo@bar.com",
+          validation: "value.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9]+$/i)",
+          errorMessage: "A valid email address is required"
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          value: "foobar",
+          validation: "value.length > 0 && value.length < 73",
+          errorMessage: "Password must be between 1 and 72 characters long"
+        }
+      }
+    }
+  }
+
+  let sandbox, footerComponent, anchors, menuitems
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create()
+
+    footerComponent = TestUtils.renderIntoDocument(<Footer />)
+    anchors = TestUtils.scryRenderedDOMComponentsWithTag(footerComponent, 'a');
+    menuitems = TestUtils.findRenderedDOMComponentWithTag(footerComponent, 'ul');
+  })
+
+  afterEach(() => {
+    sandbox.restore()
+  })
+
+  it('should generate footer block', () => {
+    expect(anchors.length).to.equal(5)
+    expect(menuitems).to.not.equal(null)
+  })
+})
